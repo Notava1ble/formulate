47286530823c705db5a9a87f7441e8926ef27d64
Visar <visarmullaj07@gmail.com>
2025-05-02 16:34:02 +0200

    added home route and updated index page to ling to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-  return <div>Main Page</div>;
+import { Button } from "@/components/ui/button";
+import { HouseIcon } from "lucide-react";
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <div className="w-screen h-screen flex-center">
+      <Link href="/home">
+        <Button>
+          <HouseIcon />
+          Go Home
+        </Button>
+      </Link>
+    </div>
+  );
 }
