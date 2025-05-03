6e594c894c519c70f56287a049f34cc62f0c9df0
Visar <visarmullaj07@gmail.com>
2025-05-03 14:07:48 +0200

    Added logic go get collection from its id in the [collectionId] route

--- src/app/home/[collectionId]/page.tsx
+++ src/app/home/[collectionId]/page.tsx
@@ -1,8 +1,22 @@
+import { redirect } from "next/navigation";
+
+import { collectionData } from "@/data/collectionData";
+import { userCollectionData } from "@/data/userCollectionData";
+
 export default async function Page({
   params,
 }: {
   params: Promise<{ collectionId: string }>;
 }) {
   const { collectionId } = await params;
-  return <div>My Post: {collectionId}</div>;
+  const intCollectionId = parseInt(collectionId);
+
+  const collection =
+    intCollectionId < 100
+      ? collectionData.find((col) => col.id == intCollectionId)
+      : userCollectionData.find((col) => col.id == intCollectionId);
+
+  if (!collection) redirect("/not-found");
+
+  return <div>Collection: {collection?.name}</div>;
 }
