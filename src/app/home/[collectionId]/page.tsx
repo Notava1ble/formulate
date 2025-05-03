9c0c746cef09267595a5fc016fc1230dd49df872
Visar <visarmullaj07@gmail.com>
2025-05-03 14:21:22 +0200

    Added elements to Colection Page

--- src/app/home/[collectionId]/page.tsx
+++ src/app/home/[collectionId]/page.tsx
@@ -2,6 +2,8 @@ import { redirect } from "next/navigation";
 
 import { collectionData } from "@/data/collectionData";
 import { userCollectionData } from "@/data/userCollectionData";
+import Navbar from "@/components/Navbar";
+import NoteCard from "@/components/NoteCard";
 
 export default async function Page({
   params,
@@ -18,5 +20,26 @@ export default async function Page({
 
   if (!collection) redirect("/not-found");
 
-  return <div>Collection: {collection?.name}</div>;
+  return (
+    <>
+      <Navbar />
+      <div className="p-6">
+        <div className=" w-full flex-center p-24 pt-32">
+          <h1 className="text-6xl font-semibold font-poppins">
+            {collection.name}
+          </h1>
+        </div>
+        {/* Premade Collections */}
+        <div className="flex-col-center mt-16">
+          <div className="w-full grid grid-cols-2 gap-8 mt-16 px-4">
+            {collection.notes.map((note) => {
+              return (
+                <NoteCard key={note.id} note={note} collection={collection} />
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
---
+++ src/components/NoteCard.tsx
@@ -0,0 +1,22 @@
+import type { CollectionType, NoteType } from "../data/collectionData";
+
+const NoteCard = ({
+  note,
+  collection,
+}: {
+  note: NoteType;
+  collection: CollectionType;
+}) => {
+  return (
+    <div className="bg-zinc-800 rounded-md p-6 py-8 flex justify-start items-center gap-8 hover:scale-102 transition-all">
+      <div>
+        <collection.icon className="size-20" />
+      </div>
+      <div>
+        <h1 className="text-3xl font-poppins font-bold">{note.name}</h1>
+        <p>{note.content.substring(0, 50)}</p>
+      </div>
+    </div>
+  );
+};
+export default NoteCard;
