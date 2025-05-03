d74987741ab87f34b580fd120d5cde7aa0f072f5
Visar <visarmullaj07@gmail.com>
2025-05-03 13:43:22 +0200

    made bg of navbar darker

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -15,7 +15,7 @@ const Page = async ({
   return (
     <>
       {/* Navbar */}
-      <div className="fixed pt-6 px-4 pb-6 w-full bg-transparent backdrop-blur-lg border-b-1">
+      <div className="fixed pt-6 px-4 pb-6 w-full bg-black/30 backdrop-blur-lg border-b-1">
         <div className="w-full flex justify-between items-center">
           <h1 className="text-lg font-poppins font-bold">Formulate</h1>
           <div className="flex-center gap-6">
