b772eba94f4d043525b82f4fa236eb02fa565584
Visar <visarmullaj07@gmail.com>
2025-05-03 14:42:30 +0200

    Added the sidebar(empty) and fixed navbar positioning

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Button } from "./ui/button";
 
 const Navbar = () => {
   return (
-    <div className="fixed pt-6 px-4 pb-6 w-full bg-black/30 backdrop-blur-lg border-b-1">
+    <div className="absolute pt-6 px-4 pb-6 w-full bg-black/30 backdrop-blur-lg border-b-1">
       <div className="w-full flex justify-between items-center">
         <h1 className="text-lg font-poppins font-bold">Formulate</h1>
         <div className="flex-center gap-6">
