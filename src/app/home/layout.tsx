a24c29ea77f5ab33d7a5517299c07e9ee87ef41b
Visar <visarmullaj07@gmail.com>
2025-05-04 13:24:27 +0200

    Added elements to the sidebar component and improved the ui

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main className="relative">{children}</main>
+      <main className="relative w-full">{children}</main>
     </SidebarProvider>
   );
 }
--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -3,16 +3,64 @@ import {
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
-  SidebarHeader,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarSeparator,
 } from "@/components/ui/sidebar";
+import { collectionData } from "@/data/collectionData";
+import { userCollectionData } from "@/data/userCollectionData";
+import Link from "next/link";
 
 export default function AppSidebar() {
   return (
-    <Sidebar variant="floating">
-      <SidebarHeader />
+    <Sidebar
+      variant="floating"
+      collapsible="icon"
+      className="overflow-x-hidden"
+    >
       <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
+        {/* USER COLLECTIONS */}
+        <SidebarGroup>
+          <SidebarGroupLabel>Your Collections</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {userCollectionData.map((collection) => (
+                <SidebarMenuItem key={collection.id}>
+                  <SidebarMenuButton asChild>
+                    <Link href={`/home/${collection.id}`}>
+                      <collection.icon />
+                      <span>{collection.name}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+
+        <SidebarSeparator />
+
+        {/* PREMADE COLLECTION */}
+        <SidebarGroup>
+          <SidebarGroupLabel>Premade Collections</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {collectionData.map((collection) => (
+                <SidebarMenuItem key={collection.id}>
+                  <SidebarMenuButton asChild>
+                    <Link href={`/home/${collection.id}`}>
+                      <collection.icon />
+                      <span>{collection.name}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
       </SidebarContent>
       <SidebarFooter />
     </Sidebar>
