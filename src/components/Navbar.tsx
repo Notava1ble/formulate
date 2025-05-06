0a427cb52edff1c02a2a1d05293bee106414ddf3
Visar <visarmullaj07@gmail.com>
2025-05-06 16:56:53 +0200

    fixed navbar positioning

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className="absolute pt-6 px-4 pb-6 w-full bg-[var(--background)]/70 backdrop-blur-lg border-b-1 z-100">
+    <div className="sticky top-0 pt-6 px-4 pb-6 w-full bg-[var(--background)]/70 backdrop-blur-md border-b-1 z-100">
       <div className="w-full flex justify-between items-center">
         <Link href={`/home`} className="no-underline">
           <h1 className="text-lg font-poppins font-bold">Formulate</h1>
