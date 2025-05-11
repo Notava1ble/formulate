d4799bd8c4d0796de5829ea733cc0e2f06ab55b5
Visar <visarmullaj07@gmail.com>
2025-05-11 12:46:03 +0200

    commented out a sidebar component temporarely

--- src/components/CollectionSidebarItem.tsx
+++ src/components/CollectionSidebarItem.tsx
@@ -1,49 +1,49 @@
-import Link from "next/link";
-import {
-  Collapsible,
-  CollapsibleContent,
-  CollapsibleTrigger,
-} from "./ui/collapsible";
-import {
-  SidebarMenuButton,
-  SidebarMenuItem,
-  SidebarMenuSub,
-  SidebarMenuSubButton,
-  SidebarMenuSubItem,
-} from "./ui/sidebar";
-import { CollectionType } from "@/data/collectionData";
+// import Link from "next/link";
+// import {
+//   Collapsible,
+//   CollapsibleContent,
+//   CollapsibleTrigger,
+// } from "./ui/collapsible";
+// import {
+//   SidebarMenuButton,
+//   SidebarMenuItem,
+//   SidebarMenuSub,
+//   SidebarMenuSubButton,
+//   SidebarMenuSubItem,
+// } from "./ui/sidebar";
+// import { CollectionType } from "@/data/collectionData";
 
-const CollectionSidebarItem = ({
-  collection,
-}: {
-  collection: CollectionType;
-}) => {
-  return (
-    <Collapsible className="group/collapsible">
-      <SidebarMenuItem>
-        <CollapsibleTrigger asChild>
-          <SidebarMenuButton asChild>
-            <div>
-              <collection.icon />
-              <span>{collection.name}</span>
-            </div>
-          </SidebarMenuButton>
-        </CollapsibleTrigger>
-        <CollapsibleContent>
-          <SidebarMenuSub>
-            {collection.subCollection.map((subCollection) => (
-              <SidebarMenuSubItem key={subCollection.id}>
-                <SidebarMenuSubButton asChild>
-                  <Link href={`/home/${collection.id}/${subCollection.id}`}>
-                    <span>{subCollection.name}</span>
-                  </Link>
-                </SidebarMenuSubButton>
-              </SidebarMenuSubItem>
-            ))}
-          </SidebarMenuSub>
-        </CollapsibleContent>
-      </SidebarMenuItem>
-    </Collapsible>
-  );
-};
-export default CollectionSidebarItem;
+// const CollectionSidebarItem = ({
+//   collection,
+// }: {
+//   collection: CollectionType;
+// }) => {
+//   return (
+//     <Collapsible className="group/collapsible">
+//       <SidebarMenuItem>
+//         <CollapsibleTrigger asChild>
+//           <SidebarMenuButton asChild>
+//             <div>
+//               <collection.icon />
+//               <span>{collection.name}</span>
+//             </div>
+//           </SidebarMenuButton>
+//         </CollapsibleTrigger>
+//         <CollapsibleContent>
+//           <SidebarMenuSub>
+//             {collection.subCollection.map((subCollection) => (
+//               <SidebarMenuSubItem key={subCollection.id}>
+//                 <SidebarMenuSubButton asChild>
+//                   <Link href={`/home/${collection.id}/${subCollection.id}`}>
+//                     <span>{subCollection.name}</span>
+//                   </Link>
+//                 </SidebarMenuSubButton>
+//               </SidebarMenuSubItem>
+//             ))}
+//           </SidebarMenuSub>
+//         </CollapsibleContent>
+//       </SidebarMenuItem>
+//     </Collapsible>
+//   );
+// };
+// export default CollectionSidebarItem;
