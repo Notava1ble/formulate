c663154605720b10b1746db189601a6ee82de9ca
Visar <visarmullaj07@gmail.com>
2025-05-14 18:59:46 +0200

    improved the skeleton to closer follow the structure of the main ui

--- src/components/LoadingCards.tsx
+++ src/components/LoadingCards.tsx
@@ -4,9 +4,17 @@ const LoadingCards = () => {
   return (
     <div className="w-full grid grid-cols-2 gap-8 mt-16 px-4">
       {Array.from({ length: 4 }).map((_, i) => (
-        <Skeleton key={i} className="py-8">
-          <div className="size-20" />
-        </Skeleton>
+        <div
+          className="bg-zinc-800 rounded-md p-6 py-8 flex justify-start items-center gap-8"
+          key={i}
+        >
+          <div>
+            <Skeleton className="bg-zinc-900 size-20" />
+          </div>
+          <div>
+            <Skeleton className="bg-zinc-900 w-48 h-6" />
+          </div>
+        </div>
       ))}
     </div>
   );
