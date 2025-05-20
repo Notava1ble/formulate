d97df087a0a27de50dc9e2bd15b7149c07e56ee1
Visar <visarmullaj07@gmail.com>
2025-05-20 19:11:05 +0200

    A small fix to show the skeletons in create loading

--- src/app/home/create/loading.tsx
+++ src/app/home/create/loading.tsx
@@ -2,15 +2,15 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 const loading = () => {
   return (
-    <div className="p-6">
+    <div className="p-6 w-full">
       <div className="w-full flex-center mt-32">
         <h1 className="text-6xl font-semibold font-poppins">
           Create a Collection
         </h1>
       </div>
-      <div className="mt-20 px-16">
-        <Skeleton className="w-full h-8px" />
-        <Skeleton className="w-full h-8px" />
+      <div className="mt-20 px-16 w-full">
+        <Skeleton className="w-full h-6 bg-zinc-800" />
+        <Skeleton className="w-full h-6 bg-zinc-800 mt-6" />
       </div>
     </div>
   );
