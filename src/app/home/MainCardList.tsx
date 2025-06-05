588118bb258409224ccacab7654cca2a19dc08b4
Visar <visarmullaj07@gmail.com>
2025-06-05 16:58:33 +0200

    fixed not show userCollections if none already

--- src/app/home/MainCardList.tsx
+++ src/app/home/MainCardList.tsx
@@ -3,6 +3,7 @@ import {
   getPremadeCollections,
 } from "@/supabase/db/collection";
 import CollectionCard from "../../components/CollectionCard";
+import { cn } from "@/lib/utils";
 
 const CardList = async () => {
   const [premadeCollections, userCollections] = await Promise.all([
@@ -10,33 +11,40 @@ const CardList = async () => {
     getCollectionsForUserId(),
   ]);
 
+  const areThereAnyUserCollections =
+    userCollections && userCollections.length > 0;
+
   return (
     <div className="flex-col-center mt-36">
-      {userCollections?.length && (
+      {areThereAnyUserCollections && (
         <>
           <h2 className="text-2xl font-poppins font-medium">
             Your Collections
           </h2>
           <div className="w-full grid grid-cols-2 gap-8 mt-8 px-4">
-            {userCollections &&
-              userCollections
-                // .filter((collection) =>
-                //   collection.name.toLocaleLowerCase().includes(stringQuery)
-                // )
-                .map((collection) => {
-                  return (
-                    <CollectionCard
-                      key={collection.id}
-                      collection={collection}
-                      isPremade={false}
-                      href={`/home/${collection.id}`}
-                    />
-                  );
-                })}
+            {userCollections
+              // .filter((collection) =>
+              //   collection.name.toLocaleLowerCase().includes(stringQuery)
+              // )
+              .map((collection) => {
+                return (
+                  <CollectionCard
+                    key={collection.id}
+                    collection={collection}
+                    isPremade={false}
+                    href={`/home/${collection.id}`}
+                  />
+                );
+              })}
           </div>
         </>
       )}
-      <h2 className="text-2xl font-poppins font-medium mt-16">
+      <h2
+        className={cn(
+          "text-2xl font-poppins font-medium",
+          areThereAnyUserCollections && "mt-16"
+        )}
+      >
         Premade Collections
       </h2>
       <div className="w-full grid grid-cols-2 gap-8 mt-8 px-4">
